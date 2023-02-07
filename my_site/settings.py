14d16abb726c3b47ff694bff595751eb4a62ec01
Thet Tun Kyaw <thettunkyaw301098@gmail.com>
2023-02-07 11:23:33 +0630

    Updated a little bit.

--- my_site/settings.py
+++ my_site/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-from os import getenv
+from os import getenv, environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a*6vdz1dx$y^fn5)!z9%e2d^oc2rs%4(5rv7_y52)_#ku%vi%-'
+# SECRET_KEY = 'django-insecure-a*6vdz1dx$y^fn5)!z9%e2d^oc2rs%4(5rv7_y52)_#ku%vi%-'
+SECRET_KEY = environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = getenv("IS_DEVELOPMENT", True)
+DEBUG = getenv("IS_DEVELOPMENT", False)
+# DEBUG = False
 
 ALLOWED_HOSTS = [
     getenv("APP_HOST")
@@ -52,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'my_site.urls'
